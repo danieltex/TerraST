7051be7ea12a7ca858add061ae98420bd8fa53d9
Daniel T Santos <danieltex@gmail.com>
2014-06-24 22:00:01 -0300

    Wrappers updated

--- src/Wrappers.cpp
+++ src/Wrappers.cpp
@@ -1,10 +1,19 @@
 #include "Wrappers.h"
+#include "STExamples.h"
+#include <terralib/st.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <locale>
 
 #define FORMAT_TIME "%Y-%b-%d %H:%M:%S"
 
 // [[Rcpp::export]]
 std::vector<Rcpp::Datetime> getTime()
 {
+
+	std::locale l("");
+	std::setlocale(LC_ALL,"en_US.UTF-8");
+
 	boost::ptr_vector<te::st::TrajectoryDataSet> outputs;
     
 	//Load the trajectories from a KML Data Source
@@ -18,13 +27,16 @@ std::vector<Rcpp::Datetime> getTime()
 	{
 //		std::auto_ptr<String> id = output->getID();
 		std::auto_ptr<te::dt::DateTime> time = output.getTime();
+		std::cout << time->toString() << std::endl;
 		Rcpp::Datetime dt(time->toString(), FORMAT_TIME);
 		dt_vec.push_back(dt);
-//		std::auto_ptr<te::gm::Geometry> geom = output->getGeometry();
+		std::auto_ptr<te::gm::Geometry> geom = output.getGeometry();
 
 //		std::cout << "Date and Time: " <<  time->toString() << std::endl;
-//		std::cout << "Geometry: " <<  geom->toString()  << std::endl << std::endl;
+		std::cout << "Geometry: " <<  geom->toString()  << std::endl << std::endl;
 	}
+	
+	std::setlocale(LC_ALL, l.name().c_str());
 	
 	return dt_vec;
 }
